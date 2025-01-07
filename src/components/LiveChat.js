45286b821e8e0cd06398a56f2fdf3944d60529ef
GaurabCodeLab <kumargaurab56@gmail.com>
2025-01-08 00:08:31 +0530

    live chat functionality added

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,14 +1,45 @@
+import { useEffect, useState, useRef } from "react";
 import { IoPersonCircleSharp } from "react-icons/io5";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [chatsDetails, setChatDetails] = useState([]);
+  const ele = useRef();
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (chatsDetails.length > 20) {
+        chatsDetails.unshift();
+      }
+      ele.current.scrollTop =
+        ele.current.scrollHeight + ele.current.clientHeight;
+      setChatDetails((pre) => {
+        return [
+          ...pre,
+          { name: generateRandomName(), message: makeRandomMessage(20) },
+        ];
+      });
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [chatsDetails]);
+
+  console.log("chat details", chatsDetails);
   return (
-    <div>
-      <div className="flex gap-1 h-full border p-4 rounded-md">
-        <IoPersonCircleSharp className="text-3xl cursor-pointer relative bottom-[2px]" />
-        <div className="font-bold">Gaurab Kumar</div>
-        <div>My first live chat message</div>
+    <div className="border">
+      <h2 className="font-bold ps-2 pb-1 text-green-500">Live Chat</h2>
+      <div
+        className="h-full overflow-auto scroll-smooth no-scrollbar"
+        ref={ele}
+      >
+        {chatsDetails.length > 0 &&
+          chatsDetails.map((value) => (
+            <div className="flex gap-1 px-4 py-1 rounded-md">
+              <IoPersonCircleSharp className="text-3xl cursor-pointer relative bottom-[2px]" />
+              <div className="font-bold">{value.name}</div>
+              <div>{value.message}</div>
+            </div>
+          ))}
       </div>
-      <div className="mt-3">
+      <div>
         <input className="border ps-2 py-1 me-3" />
         <button className="border py-1 px-3 rounded-sm">Send</button>
       </div>
